ea0868a05c417a14e21c2eb42ec1d74f8e50cfa6
joeldaruvalla <uketutseasy@gmail.com>
2014-12-03 19:14:29 +0000

    Update read_lines.py

--- read_lines.py
+++ read_lines.py
@@ -1,12 +1,24 @@
-""" Doc String	"""
+""" This program prints the first station of the line passed in - Not finished """
 
-from xml.dom.minidom import parse, parseString
+import xml.dom.minidom
 
-def read_lines():
-	"""	"""
-	datasource = open('stations.xml')
-	dom  = parse(datasource)
+def read_lines(line_id):
 
-	return dom
+	dom = xml.dom.minidom.parse('lines.xml')
+	a = dom.getElementsByTagName("line")
+	#b = a[0].attributes["id"].value
+
+	for tag in a:
+
+		lineID = tag.attributes["id"].value
+
+		if lineID == line_id:
+
+			print(tag.getElementsByTagName("station1")[0].firstChild.nodeValue)
+
 
 if __name__ == "__main__":
+	
+	read_lines("Circle")
+
+	
