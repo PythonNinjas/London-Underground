c2b155c24eec4c36117f75ad16aae21c73ac1893
joeldaruvalla <uketutseasy@gmail.com>
2014-12-03 17:05:00 +0000

    Update read_lines.py

--- read_lines.py
+++ read_lines.py
@@ -1,3 +1,12 @@
-read_lines():
-	“””
-	“””
+""" Doc String	"""
+
+from xml.dom.minidom import parse, parseString
+
+def read_lines():
+	"""	"""
+	datasource = open('stations.xml')
+	dom  = parse(datasource)
+
+	return dom
+
+if __name__ == "__main__":
