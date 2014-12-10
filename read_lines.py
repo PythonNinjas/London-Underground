80d628f662ea5c1e24a1300bfd9ad919fd5ebed2
joeldaruvalla <uketutseasy@gmail.com>
2014-12-10 18:39:57 +0000

    Update read_lines.py

--- read_lines.py
+++ read_lines.py
@@ -14,21 +14,32 @@ def read_lines():
 	for line in root.findall('line'):
 
 		line_name = line.get('id')
-
 		stations = []
 
 		for station in line.getchildren():
 
-			stations.append(station.text)
+			if "part" in station.tag:
+				stations.append(get_part_contents(station))
+			else:
+				stations.append(station.text.replace("\"", ""))
 
 		lines[line_name] = stations
 
 	return lines
 
 
+def get_part_contents(part):
+
+	""" Returns stations in part tag as list """
+
+	stations = []
+
+	for station in part.getchildren():
+
+		stations.append(station.text.replace("\"", ""))
+
+	return stations
+
 if __name__ == "__main__":
 
 	d = read_lines()
-	print(d)
-
-	
